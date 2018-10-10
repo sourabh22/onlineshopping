608137e86c246a7b994161c0c62ee74095349f29
noopur <noopursharma35@gmail.com>
2018-10-10 12:28:47 +0530

    test case commit

--- OnlineShoppingTest/UnitTest1.cs
+++ OnlineShoppingTest/UnitTest1.cs
@@ -1,13 +1,49 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OnlineShoppingService.Controllers;
+using OnlineShoppingService.Models.DB;
 
-namespace OnlineShoppingTest
+namespace OnlineShoppingTests
 {
     [TestClass]
-    public class UnitTest1
+    public class OnlineShoppintTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        SearchController controller;
+        OnlineShoppingDbContext context;
+        public OnlineShoppintTests()
         {
+            context = new OnlineShoppingDbContext();
+            controller = new SearchController(context);
+        }
+        [TestMethod]
+        public void GetCategoryTest()
+        {
+            var result=controller.GetCategories();
+            Assert.IsNotNull(result);
+        }
+        [TestMethod]
+        [TestCategory("SubcategoryTest")]
+        public void GetSubCategoryOnValidcategoryIdTest()
+        {
+            var result = controller.GetSubCategories(100);
+            Assert.IsNotNull(result);
+        }
+        [TestMethod]
+        [TestCategory("SubcategoryTest")]
+        public void GetSubCategoryOnInvalidcategoryIdTest()
+        {
+            var result = controller.GetSubCategories(1000);
+            Assert.AreEqual(0, result.Count);
+        }
+        [TestMethod]
+      
+        public void GetProductsTest()
+        {
+            SubCategory subCategory = new SubCategory() {
+                 CategoryId=101,
+                 SubCategoryId=105
+            };
+            var result = controller.GetProducts(subCategory);
+            Assert.AreEqual(2, result.Count);
         }
     }
 }
